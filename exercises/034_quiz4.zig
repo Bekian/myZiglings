bf9c7527b174658f39dbf2762d8eb1b361f32cd0
Bekian <Bekian.Cat@gmail.com>
2024-05-16 22:02:41 -0500

    fix(app): fixed 28-38
    see changes

--- exercises/034_quiz4.zig
+++ exercises/034_quiz4.zig
@@ -8,17 +8,27 @@
 const std = @import("std");
 
 const NumError = error{IllegalNumber};
-
-pub fn main() void {
+// i left my original solution here so i can see how i solved it originally
+// the correct solution is in comments and the patch
+pub fn main() void { // should be !void
     const stdout = std.io.getStdOut().writer();
 
-    const my_num: u32 = getNumber();
+    const my_num = getNumber() catch 42;
+    // should be:
+    // const my_num = try getNumber() 42;
 
-    try stdout.print("my_num={}\n", .{my_num});
+    // the below line is unnecessary
+    errdefer std.debug.print("my_num=42", .{});
+    stdout.print("my_num={}\n", .{my_num}) catch std.debug.print("my_num=42", .{});
+    // should be:
+    // try stdout.print("my_num={}\n", .{my_num})
 }
 
 // This function is obviously weird and non-functional. But you will not be changing it for this quiz.
 fn getNumber() NumError!u32 {
+    // this expression is weird
+    // my guess is that the expression is false, and therefore is true, thus resulting in the error,
+    // idk why true isnt used for clarity
     if (false) return NumError.IllegalNumber;
     return 42;
 }
